4fe86605194aa276b19505da6cb9bada57c6fabd
takkanm <takkanm@gmail.com>
2018-04-19 22:31:04 +0900

    remove unnecessary spec

--- spec/ggggsss_spec.rb
+++ spec/ggggsss_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Ggggsss do
   it "has a version number" do
     expect(Ggggsss::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
